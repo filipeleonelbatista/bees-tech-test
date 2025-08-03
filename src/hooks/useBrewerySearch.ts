97135f94bf1320510b99a30b66125e8dd0d39192
Filipe de Leonel Batista <filipe.x@live.com>
2025-08-03 20:56:46 -0300

    fix(useBrewerySearch): add conditional proxy url for dev environment
    The proxy url is now only used in development mode to avoid unnecessary requests in production

--- src/hooks/useBrewerySearch.ts
+++ src/hooks/useBrewerySearch.ts
@@ -6,6 +6,8 @@ export interface Brewery {
     [key: string]: any;
 }
 
+const proxy_url = import.meta.env.DEV ? 'https://cors-anywhere-filipeleonelbatista.onrender.com/' : '';
+
 export const useBrewerySearch = () => {
     const [search, setSearch] = useState('');
     const [results, setResults] = useState<Brewery[]>([]);
@@ -15,7 +17,7 @@ export const useBrewerySearch = () => {
         setLoading(true)
         try {
             const res = await fetch(
-                `https://cors-anywhere-filipeleonelbatista.onrender.com/https://api.openbrewerydb.org/v1/breweries/search?per_page=6&query=${encodeURIComponent(search)}`
+                `${proxy_url}https://api.openbrewerydb.org/v1/breweries/search?per_page=6&query=${encodeURIComponent(search)}`
             );
             const data = await res.json();
             setResults(data);
